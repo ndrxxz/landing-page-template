58f8b8f73d02c29f54381e02783163a610dbc3a0
Andrea <andreatorres23xxx@gmail.com>
2026-09-07 01:57:52 +0800

    refactor: update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+
+import "./index.css";
+import App from "./App.tsx";
+import { HelmetProvider } from "react-helmet-async";
+
+createRoot(document.getElementById("root")!).render(
+  <HelmetProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </HelmetProvider>,
+);
