306efee1aace3e233dcbe9738b27b1891fc134e6
Andrea <andreatorres23xxx@gmail.com>
2026-09-09 07:04:51 +0800

    feature: create bg color component

---
+++ src/components/BgColor.ts
@@ -0,0 +1,8 @@
+import { Color } from "@/components";
+
+export const BgColor = {
+  info: Color.info[100],
+  success: Color.success[100],
+  warning: Color.warning[100],
+  danger: Color.danger[100],
+} as const;
